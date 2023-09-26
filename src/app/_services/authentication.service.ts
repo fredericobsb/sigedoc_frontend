072a3edf7c4d9121d0d7d39bc9cf8b9db68a4c68
frederico <fredyramos0707@gmail.com>
2023-09-26 08:41:10 -0300

    ajustes finais

--- src/app/_services/authentication.service.ts
+++ src/app/_services/authentication.service.ts
@@ -27,7 +27,6 @@ export class AuthenticationService {
                 // store user details and jwt token in local storage to keep user logged in between page refreshes
                 localStorage.setItem('currentUser', JSON.stringify(user));
                 localStorage.setItem('token', user.accessToken);
-                console.log('------------token em AUTHENTICATION SERVICE ===> ', localStorage.token);
                 this.currentUserSubject.next(user);
                 return user;
             }));
